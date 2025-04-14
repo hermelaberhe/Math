c50d0036d8494b65a516e04c9b8dbed062f3d269
hermberhe <hermberhe@gmail.com>
2025-04-13 22:50:14 -0700

    Initial commit for Math project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double degrees = 90.0;
+        double answer = Math.sin(degrees);
+        System.out.println(answer);
+
+        double value = 1234.567;
+        long wholeNumber = Math.round(value); //round up if fraction is 0.5 or greater
+        System.out.println(wholeNumber);
+        System.out.println(Math.ceil(value)); //round up no matter what
+        System.out.println(Math.floor(value)); //round down no matter what
+
+    }
+}
